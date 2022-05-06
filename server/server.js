48ecf6cfb43f145c79efa4351288d4b1eb8c6941
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-06 23:25:41 +0300

    Mongodb sunucusundan veri çekiliyor

--- server/server.js
+++ server/server.js
@@ -1,27 +1,10 @@
 const { ApolloServer } = require('apollo-server')
 const gql = require('graphql-tag') // typeDefs tanımları için gerekli
 
-const { MongoClient, ServerApiVersion } = require('mongodb')
-const uri =
-  'mongodb+srv://serihesap:blog1234@cluster0.kp9ua.mongodb.net/blogDB?retryWrites=true&w=majority'
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-})
+const client = require('mongoose')
+const { ServerApiVersion } = require('mongodb')
 
-client
-  .connect()
-  .then(() => {
-    console.log(`Mongo db bağlantısı başarılı \n`)
-    return server.listen({ port: 4000 })
-  })
-  .then((result) => {
-    console.log(`server ${result.url} adresinde ${result.port} portta çalışıyor`)
-  })
-  .catch((err) => {
-    console.log(`Server ${err} hatası veriyor`)
-  })
+const MakaleModel = require('./model/MakaleModel')
 
 const typeDefs = gql`
   type Makale {
@@ -37,18 +20,44 @@ const typeDefs = gql`
 
 const resolvers = {
   Query: {
-    makalelerGetir() {
-      const makaleler = [
-        { id: 1, baslik: 'Makale1 başlık1', icerik: 'Makale1 içerik' },
-        { id: 2, baslik: 'Makale2 başlık2', icerik: 'Makale2 içerik' },
-        { id: 3, baslik: 'Makale3 başlık3', icerik: 'Makale3 içerik' },
-      ]
+    async makalelerGetir() {
+      const makaleler = await MakaleModel.find()
       return makaleler
     },
+
+    // makalelerGetir() {
+    //   const makaleler = [
+    //     { id: 1, baslik: 'Makale1 başlık1', icerik: 'Makale1 içerik' },
+    //     { id: 2, baslik: 'Makale2 başlık2', icerik: 'Makale2 içerik' },
+    //     { id: 3, baslik: 'Makale3 başlık3', icerik: 'Makale3 içerik' },
+    //   ]
+    //   return makaleler
+    // },
   },
-}
+};
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
+
+const uri =
+  'mongodb+srv://serihesap:blog1234@blogcluster.kp9ua.mongodb.net/blogDB?retryWrites=true&w=majority'
+
+client
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  })
+  .then(() => {
+    console.log(`Mongodb bağlantısı başarılı \n`)
+    return server.listen({ port: 4000 })
+  })
+  .then((result) => {
+    console.log(`server ${result.url} adresinde ${result.port} portta çalışıyor`)
+  })
+  .catch((err) => {
+    console.log(`Server ${err} hatası veriyor`)
+  })
+
