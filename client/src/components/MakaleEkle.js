b45bbfc2d1c9c92c8c37ab1b4d55a7707d087408
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-07 22:50:39 +0300

    Makale eklem için form oluşturuldu

--- client/src/components/MakaleEkle.js
+++ client/src/components/MakaleEkle.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function MakaleEkle() {
+
+  const [veriler, setVeriler] = useState({
+      baslik: '',
+      icerik: ''
+  });
+
+  const onChange = (e) => {
+      setVeriler({ ...veriler, [e.target.name]:e.target.value })
+  }
+
+  const onSubmit = (e) => {
+      e.preventDefault();
+      console.log(veriler);
+  }
+
   return (
-    <div>MakaleEkle</div>
+    <div className='makale-olustur'>
+      <form onSubmit={onSubmit}>
+        <label htmlFor='baslik'>Makale Başlık:</label>
+        <input type='text' id='baslik' name='baslik' onChange={onChange}></input>
+        <label htmlFor='icerik'>Makale İçeriği:</label>
+        <textarea type='text' id='icerik' name='icerik' onChange={onChange}></textarea>
+        <button>Kaydet</button>
+      </form>
+    </div>
   )
 }
