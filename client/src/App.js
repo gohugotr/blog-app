47082e937e9ba05307674c3e7f96dfe074e322ae
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-07 21:00:39 +0300

    Apolloclient hizmete alındı

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,13 @@ import MakaleListesi from './components/MakaleListesi'
 import Baslik from './components/Baslik'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import ApolloClient from "apollo-boost"
+import {ApolloProvider} from '@apollo/react-hooks'
+
+
+const client = new ApolloClient({
+  uri:'http://localhost:4000/'
+})
 
 function App() {
       // const Ekle = () => {
@@ -15,16 +22,18 @@ function App() {
         return <h3>404 Hata. Sayfa bulunamadı.</h3>
       }
   return (
-    <Router>
-      <div className='App'>
-        <Baslik/>
-        <Routes>
-          <Route path='/' exact element={<MakaleListesi />} />
-          <Route path='/ekle' element={<MakaleEkle />} />
-          <Route path='*' element={<Error />} />
-        </Routes>
-      </div>
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div className='App'>
+          <Baslik />
+          <Routes>
+            <Route path='/' exact element={<MakaleListesi />} />
+            <Route path='/ekle' element={<MakaleEkle />} />
+            <Route path='*' element={<Error />} />
+          </Routes>
+        </div>
+      </Router>
+    </ApolloProvider>
   )
 }
 
