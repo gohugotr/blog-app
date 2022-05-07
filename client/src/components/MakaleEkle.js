62cc5bc983d569ddfe39e10bc1cc9c8400365e44
Cengiz YILMAZ <terabilgi@gmail.com>
2022-05-08 00:16:26 +0300

    .

--- client/src/components/MakaleEkle.js
+++ client/src/components/MakaleEkle.js
@@ -1,4 +1,14 @@
 import React, { useState } from 'react'
+import {gql, useMutation} from "@apollo/client";
+
+const MAKALE_EKLE = gql`
+
+    mutation makaleEkle($baslik: String!, $icerik: String!){
+      makaleOlustur(baslik:$baslik, icerik:$icerik){
+        id, baslik, icerik
+      }
+    }
+`;
 
 export default function MakaleEkle() {
 
@@ -7,13 +17,22 @@ export default function MakaleEkle() {
       icerik: ''
   });
 
+  const [makaleEkle, {loading}] = useMutation(MAKALE_EKLE,{
+    update(proxy, sonuc){
+      console.log(sonuc);
+    },
+    variables: veriler
+  })
+
   const onChange = (e) => {
       setVeriler({ ...veriler, [e.target.name]:e.target.value })
   }
 
   const onSubmit = (e) => {
       e.preventDefault();
-      console.log(veriler);
+      //console.log(veriler);
+      makaleEkle();
+      window.location = '/';
   }
 
   return (
